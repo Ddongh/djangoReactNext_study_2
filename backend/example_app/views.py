bc2e64cb413f4240bf46e8b0552830e87555bcf6
Ddongh <qwer5383@naver.com>
2023-08-21 22:32:26 +0900

    .

---
+++ backend/example_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+@api_view(['GET', "POST"])
+@permission_classes([AllowAny])
+def hello_rest_api(request):
+    data = {'message' : "Hello, REST API!!!"}
+    return Response(data)
+
+def hello(request):
+    return HttpResponse("Hello, World!")
